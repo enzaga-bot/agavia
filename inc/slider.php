1e402b675998ef6ce620b96ab27beef862f7aee9
Enrique Zavala Garcia <contactoenriquezavala@gmail.com>
2025-11-28 08:15:47 -0600

    Slider con efectos y con textos funcional en modo escritorio

--- inc/slider.php
+++ inc/slider.php
@@ -1,42 +1,86 @@
 	<article id="contieneSlider" class="contieneSeccion">
 
-        <a class="slider" id="slider01" href="">
+        <div class="slider" id="slider01">
+            <div id="textoDiapo01" class="contieneTexto">
+                <h2>AGAVIA</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates mollitia qui odit nemo explicabo porro officia</p>
+                <div class="boton dorado"><a href="">Read More</a></div>
+            </div><!--contieneTexto-->
             <picture>
                 <source media="(min-width: 900px)" srcset="images/slide01.webp" loading="lazy" style="width:100%">
                 <source media="(min-width: 550px)" srcset="images/slide01_med.webp">
                 <source srcset="images/slide01_ch.webp">
                 <img src="images/slide01.webp" name="slider" alt="" style="width:100%">
             </picture>
-        </a>
+        </div>
 
-        <a class="slider diapoMovil" id="slider02" href="">
+        <div class="slider toque-slider02 diapoMovil" id="slider02">
+               <div class="contieneTexto">
+                    <h2>AGAVIA 2</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates mollitia qui odit nemo explicabo porro officia</p>
+                    <div class="boton dorado"><a href="">Read More</a></div>
+                </div><!--contieneTexto-->
             <picture>
                 <source media="(min-width: 900px)" srcset="images/slide02.webp" loading="lazy" style="width:100%">
                 <source media="(min-width: 550px)" srcset="images/slide02_med.webp">
                 <source srcset="images/slide02_ch.webp">
                 <img src="images/slide02.webp" name="slider" alt="" style="width:100%">
             </picture>
-        </a>
+        </div>
 
-        <a class="slider diapoMovil" id="slider03" href="">
+        <div class="slider toque-slider03 diapoMovil" id="slider03">
+                <div class="contieneTexto">
+                    <h2>AGAVIA 3</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates mollitia qui odit nemo explicabo porro officia</p>
+                    <div class="boton dorado"><a href="">Read More</a></div>
+                </div><!--contieneTexto-->
             <picture>
                 <source media="(min-width: 900px)" srcset="images/slide03.webp" loading="lazy" style="width:100%">
                 <source media="(min-width: 550px)" srcset="images/slide03_med.webp">
                 <source srcset="images/slide03_ch.webp">
                 <img src="images/slide03.webp" name="slider" alt="" style="width:100%">
             </picture>
-        </a>
+        </div>
         
-        <a class="slider diapoMovil" id="slider04" href="">
+        <div class="slider toque-slider04 diapoMovil" id="slider04">
+                <div class="contieneTexto">
+                    <h2>AGAVIA 4</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates mollitia qui odit nemo explicabo porro officia</p>
+                    <div class="boton dorado"><a href="">Read More</a></div>
+                </div><!--contieneTexto-->
             <picture>
                 <source media="(min-width: 900px)" srcset="images/slide04.webp" loading="lazy" style="width:100%">
                 <source media="(min-width: 550px)" srcset="images/slide04_med.webp">
                 <source srcset="images/slide04_ch.webp">
                 <img src="images/slide04.webp" name="slider" alt="" style="width:100%">
             </picture>
-        </a>
+        </div>
 
 	</article><!--contieneSeccion-->
 
-
     
+
+<script>
+  const textoDiapo01 = document.getElementById('textoDiapo01');
+
+  const sliders = document.querySelectorAll('#slider02, #slider03, #slider04');
+
+  function activarTexto() {
+    textoDiapo01.classList.add('mueveTextoDiapo01');
+  }
+
+  function desactivarTexto() {
+    textoDiapo01.classList.add('regresaTextoDiapo01');
+    textoDiapo01.classList.remove('mueveTextoDiapo01');
+    
+    setTimeout(function () {
+    textoDiapo01.classList.remove('regresaTextoDiapo01');
+    }, 1000);
+        
+  }
+
+  sliders.forEach(slider => {
+    slider.addEventListener('mouseenter', activarTexto);
+    slider.addEventListener('mouseleave', desactivarTexto);
+  });
+</script>
