2b8d807f7457d1c55c6d9b21572860994ccdf8f9
Enrique Zavala Garcia <contactoenriquezavala@gmail.com>
2025-12-01 22:10:54 -0600

    Responsividad aplicada a todas las secciones excepto slider y navbar

--- inc/cocktails.php
+++ inc/cocktails.php
@@ -15,23 +15,23 @@
             <div class="setenta">
                 <h2 id="tituloCoktail">COKTAILS</h2>
                 <h4 id="nombreCoktailBg">Smoky Belle Paloma</h4>
-                <img id="cocktailBg" src="images/coktailsBg01.webp" alt="">
+                <img load="lazy" id="cocktailBg" src="images/coktailsBg01.webp" alt="">
             </div><!--setenta-->
             <div class="treinta">
                 <div class="cocktailSm">
-                    <img onclick='cambiarCoktail(1, "Smoky Belle Paloma")' src="images/coktailSm01.webp" alt="">
+                    <img load="lazy" onclick='cambiarCoktail(1, "Smoky Belle Paloma")' src="images/coktailSm01.webp" alt="">
                     <div class="tituloCoktailSm">Smoky Belle Paloma</div>
                 </div><!--cocktailSm-->
                  <div class="cocktailSm">
-                    <img onclick='cambiarCoktail(2, "Rosemay Winter")' src="images/coktailSm02.webp" alt="">
+                    <img load="lazy" onclick='cambiarCoktail(2, "Rosemay Winter")' src="images/coktailSm02.webp" alt="">
                     <div class="tituloCoktailSm">Rosemay Winter</div>
                 </div><!--cocktailSm-->
                  <div class="cocktailSm">
-                    <img onclick='cambiarCoktail(3, "Moody Cocktail Shoot")' src="images/coktailSm03.webp" alt="">
+                    <img load="lazy" onclick='cambiarCoktail(3, "Moody Cocktail Shoot")' src="images/coktailSm03.webp" alt="">
                     <div class="tituloCoktailSm">Moody Cocktail Shoot</div>
                 </div><!--cocktailSm-->
                  <div class="cocktailSm">
-                    <img onclick='cambiarCoktail(4, "Margarita Pastor")' src="images/coktailSm04.webp" alt="">
+                    <img load="lazy" onclick='cambiarCoktail(4, "Margarita Pastor")' src="images/coktailSm04.webp" alt="">
                     <div class="tituloCoktailSm">Margarita Pastor</div>
                 </div><!--cocktailSm-->
             </div><!--treinta-->
--- inc/rrss.php
+++ inc/rrss.php
@@ -4,35 +4,35 @@
         <div class="contieneCuartos">
             <div class="cuarto">
                 <a href="">
-                    <img src="img/iconFB.webp" alt="Facebook">
+                    <img load="lazy" src="img/iconFB.webp" alt="Facebook">
                 </a>
             </div><!--cuarto-->
             <div class="cuarto">
                 <a href="">
-                    <img src="img/iconIG.webp" alt="Instagram">
+                    <img load="lazy" src="img/iconIG.webp" alt="Instagram">
                 </a>
             </div><!--cuarto-->
             <div class="cuarto">
                 <a href="">
-                    <img src="img/iconTikTok.webp" alt="TikTok">
+                    <img load="lazy" src="img/iconTikTok.webp" alt="TikTok">
                 </a>
             </div><!--cuarto-->
             <div class="cuarto">
                 <a href="">
-                    <img src="img/iconWA.webp" alt="WhatsApp">
+                    <img load="lazy" src="img/iconWA.webp" alt="WhatsApp">
                 </a>
             </div><!--cuarto-->
         </div><!--contieneCuartos-->
         <br>
             <div class="contieneTercios">
                 <div class="tercio">
-                    <img src="images/post01.webp" alt="">
+                    <img load="lazy" src="images/post01.webp" alt="">
                 </div><!--tercio-->
                 <div class="tercio">
-                    <img src="images/post02.webp" alt="">
+                    <img load="lazy" src="images/post02.webp" alt="">
                 </div><!--tercio-->
                 <div class="tercio">
-                    <img src="images/post03.webp" alt="">
+                    <img load="lazy" src="images/post03.webp" alt="">
                 </div><!--tercio-->
             </div><!--contieneTercios-->
     </div><!--cajaRRSS-->
